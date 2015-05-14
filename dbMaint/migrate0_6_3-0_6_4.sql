8f6cb24d3982beb8e631858f3c966904e2b6828f
Joanne Bogart <jrb@slac.stanford.edu>
2015-05-14 15:43:01 -0700

    need new row in DbRelease for 0.6.4

--- dbMaint/migrate0_6_3-0_6_4.sql
+++ dbMaint/migrate0_6_3-0_6_4.sql
@@ -5,3 +5,5 @@ alter table JobHarness add jhStageRoot varchar(255) NOT NULL after jhOutputRoot;
 insert into PrerequisiteType set name='PREPARATION', createdBy='jrb', creationTS=UTC_TIMESTAMP();
 
 insert into HardwareStatus set name='NON-COMPLIANT', description='part is not fully compliant', createdBy='jrb', creationTS=UTC_TIMESTAMP();
+
+INSERT into DbRelease  (major, minor, patch, status, createdBy, creationTS, lastModTS, remarks) values (0, 6, 4, 'TEST', 'jrb', UTC_TIMESTAMP(), UTC_TIMESTAMP(), 'Add NON-COMPLIANT hardware status; PREPARATION prereq type, tweaks to JobHarness table');
