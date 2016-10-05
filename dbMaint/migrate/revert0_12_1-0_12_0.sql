ad8cc3a7ade8dbea3da36f972664ba65827a22dd
Joanne Bogart <jrb@slac.stanford.edu>
2016-10-05 12:45:36 -0700

    fix typo

--- dbMaint/migrate/revert0_12_1-0_12_0.sql
+++ dbMaint/migrate/revert0_12_1-0_12_0.sql
@@ -1,3 +1,3 @@
 alter table RunNumber drop index ix373;
 alter table RunNumber drop runInt;
-delete from DbRelease where majore='0' and minor='12' and patch='1';
+delete from DbRelease where major='0' and minor='12' and patch='1';
